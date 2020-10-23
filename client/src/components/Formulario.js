bdef17021c5809bf7d6c6ee59d8ac28b17afdd1c
Daniel <danisan9490@gmail.com>
2020-10-23 09:58:18 +0100

    #2 Add Form

--- client/src/components/Formulario.js
+++ client/src/components/Formulario.js
@@ -1,7 +1,53 @@
 import React from 'react';
 
 const Formulario = () => {
-  return (<h1>Formulario</h1>);
+  return (
+    <div className="bg-info">
+      <div className="container">
+        <div className="row">
+          <form
+            className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+          >
+            <fieldset>
+              <legend className="text-center">Search Lyrics and Songs</legend>
+
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Artist</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="artista"
+                      placeholder="Artist Name"
+                    />
+                  </div>
+
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>Song</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="cancion"
+                      placeholder="Song Name"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary float-right"
+              >Search</button>
+            </fieldset>
+
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Formulario; 
