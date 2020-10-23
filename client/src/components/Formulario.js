c5969f410143aeb0ca94f184d94e2a60b7adc5fc
Daniel <danisan9490@gmail.com>
2020-10-23 09:59:48 +0100

    #3 Add Form State

--- client/src/components/Formulario.js
+++ client/src/components/Formulario.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+  const [busqueda, guardarBusqueda] = useState({
+    artista: '',
+    cancion: ''
+  });
+  const [error, guardarError] = useState(false);
+
+  const { artista, cancion } = busqueda;
+
+  // función a cada input para leer su contenido
+  const actualizarState = e => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // consultar las apis
+  const buscarInformacion = e => {
+    e.preventDefault();
+
+    if (artista.trim() === '' || cancion.trim() === '') {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+    // Todo bien, pasar al componente principal
+
+  }
+
+
   return (
     <div className="bg-info">
       <div className="container">
         <div className="row">
           <form
+            onSubmit={buscarInformacion}
             className="col card text-white bg-transparent mb-5 pt-5 pb-2"
           >
             <fieldset>
@@ -20,6 +52,8 @@ const Formulario = () => {
                       className="form-control"
                       name="artista"
                       placeholder="Artist Name"
+                      onChange={actualizarState}
+                      value={artista}
                     />
                   </div>
 
@@ -32,6 +66,8 @@ const Formulario = () => {
                       className="form-control"
                       name="cancion"
                       placeholder="Song Name"
+                      onChange={actualizarState}
+                      value={cancion}
                     />
                   </div>
                 </div>
