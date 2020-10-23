97c2a1004095e2ce6073b4d74a6a0155c772fd9b
Daniel <danisan9490@gmail.com>
2020-10-23 09:55:04 +0100

    #1 Install React and clean

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Formulario from './components/Formulario';
+function App() {
+  return (
+    <Fragment>
+      <Formulario />
+    </Fragment>
+  );
+}
+
+export default App;
